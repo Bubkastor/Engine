89edf1969b0e81ca932c99b7528cf0e33b41d752
Albert <bubokastor@mail.ru>
2016-02-11 19:05:58 +0300

    Template Engine

---
+++ Engine/EngineMain.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+
+// Прорисовывает содержимое Direct2D и 3D на экране.
+namespace Engine
+{
+	class EngineMain : public DX::IDeviceNotify
+	{
+	public:
+		EngineMain(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~EngineMain();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// Кэшированный указатель на ресурсы устройства.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+
+		// Таймер цикла прорисовки.
+		DX::StepTimer m_timer;
+	};
+}
